8ec73c2e4a2d538c6dfe8151917b00ac9e1c578a
Maslova Elena <alena138076@mail.ru>
2014-05-30 14:46:49 +0700

    Переделали

--- TicTakToeGame_0.2/TicTakToeGame/ViewModels/ViewModelBase.cs
+++ TicTakToeGame_0.2/TicTakToeGame/ViewModels/ViewModelBase.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace TicTakToeGame.ViewModels
 {
@@ -16,10 +17,16 @@ namespace TicTakToeGame.ViewModels
         /// Метод определяюший имя по Lambda выражению
         public void NotifyOnPropertyChange<T>(Expression<Func<T>> expression)
         {
-            /// Получаем тело выражения, из которого берем имя свойства
-            dynamic l = expression.Body;
-            string propertyName = l.Member.Name;
+            if (expression == null) throw new ArgumentNullException("expression");
+            var memberExpression = expression.Body as MemberExpression;
+            if (memberExpression == null) throw new ArgumentException("Invalid lambda expression.", "expression");
+            var property = memberExpression.Member as MemberInfo;
+            if (property == null) throw new ArgumentException("Invalid lambda expression. Expression must be a property.", "expression");
+
+
+            string propertyName = memberExpression.Member.Name;
             OnPropertyChanged(propertyName);
+
         }
 
         public void OnPropertyChanged(string propertyName)
